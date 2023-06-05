dd92b11024f11f0a06ba49a36e0ef66896021459
Diego Prieto <diego.prieto.dev@gmail.com>
2023-06-05 16:35:20 +0200

    Deploying

--- src/App.js
+++ src/App.js
@@ -5,25 +5,18 @@ import Liked from "./pages/Liked";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import {
-  HashRouter,
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div id="root">
-      <Router basename="Image-Selector">
+      <Router basename="/Image-Selector">
         <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/liked" element={<Liked />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/liked" element={<Liked />} />
+        </Routes>
       </Router>
     </div>
   );
